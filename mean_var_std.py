172bed507a266438712bf0132c44822034b2fd58
nor5 <noorbelkacem5@gmail.com>
2022-12-26 12:34:24 +0100

    firstcommit

---
+++ mean_var_std.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def calculate(list):
+  arr = np.array(list).reshape(3,3)
+  print(arr)
+  calculations = {
+  'mean': [axis1, axis2, flattened],
+  'variance': [axis1, axis2, flattened],
+  'standard deviation': [axis1, axis2, flattened],
+  'max': [axis1, axis2, flattened],
+  'min': [axis1, axis2, flattened],
+  'sum': [axis1, axis2, flattened]
+}
+
+
+
+  return calculations
